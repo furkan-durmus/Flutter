3be2a3cdb549c7ec68259c39ebe358f44fc23a27
Furkan Durmus <16furkandurmus@gmail.com>
2020-12-24 15:43:37 +0300

    v05

--- lib/main.dart
+++ lib/main.dart
@@ -8,37 +8,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Flutter Demo",
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            kimlik(
-              isim: "Furkan",
-              soyad: "Durmuş",
-              yas: 25,
-            ),
-            kimlik(
-              isim: "Ferit",
-              soyad: "Durmuş",
-              yas: 32,
-            )
-          ],
-        ),
-      ),
+      home: Sayac(isim: "Bardak"),
     );
   }
 }
 
-class kimlik extends StatelessWidget {
+class Sayac extends StatefulWidget {
   final String isim;
-  final String soyad;
-  final int yas;
-  kimlik({this.isim, this.soyad, this.yas});
+  Sayac({this.isim});
+  @override
+  _SayacState createState() => _SayacState();
+}
+
+class _SayacState extends State<Sayac> {
+  int sayi = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Text("İsim : $isim Soyad : $soyad  Yas : $yas");
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("${widget.isim} sayısı : $sayi"),
+      floatingActionButton: FloatingActionButton(
+          onPressed:
+              sayiarttir), //   bu kısımda () a sağ tıkladık, refactor dedik, extract metot ile verdiğimiz isimde bir metot oluşturdu. ilk hali anonim fonksiyon içindeydi. onPressed (){sayiarttir();} da olurdu,onPressed ()=>sayiarttir()   da olur
+    );
+  }
+
+  void sayiarttir() {
+    print("Butona Basıldı(debug consolda yazdırır.)");
+    setState(() {
+      //setstate den önce de sayi ++ yazsak çalışırdı. Önemli olan setstate ile builde değişikliği haber vermek.
+      sayi++;
+      print(sayi);
+    });
   }
 }
