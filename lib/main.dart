ed8fd5480f1206096945c7e2df71f3767fa16221
Furkan Durmus <16furkandurmus@gmail.com>
2020-12-24 14:18:24 +0300

    v02

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.teal[300],
-    child: Center(
-      child: Text(
-        "Hello  World!",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.amber, fontSize: 42.0),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(child: Text("Anasayfa")),
       ),
-    ),
-  ));
+    );
+  }
 }
